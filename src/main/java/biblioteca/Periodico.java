25e7143035903a9df611c996792bd4c1d4cb0dd0
06black11 <139305811+06black11@users.noreply.github.com>
2025-09-15 21:05:04 -0500

    Update Periodico.java

--- src/main/java/biblioteca/Periodico.java
+++ src/main/java/biblioteca/Periodico.java
@@ -2,23 +2,23 @@ package biblioteca;
 
 public class Periodico extends Recurso {
     private String editorial;
-    private int ano;
+    private int anio;
 
-    public Periodico(Integer id, String nombre, String editorial, int ano) {
+    public Periodico(Integer id, String nombre, String editorial, int anio) {
         super(id, nombre);
         this.editorial = editorial;
-        this.ano = ano;
+        this.anio = anio;
     }
 
     public String getEditorial() { return editorial; }
-    public int getAno() { return ano; }
+    public int getAnio() { return anio; }
 
     @Override
     public String toString() {
         return "Periodico [id=" + getId() +
                 ", nombre=" + getNombre() +
                 ", editorial=" + editorial +
-                ", año=" + ano +
+                ", año=" + anio +
                 ", fechaRegistro=" + getFechaRegistro() + "]";
     }
 }
